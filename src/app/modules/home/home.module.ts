43ff8c9b61bbd12528b58e3218aef6ccb1d5b3af
akshayn10 <thiruakshayan98@gmail.com>
2022-02-10 03:04:39 +0530

    home module created

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutes } from './home-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeRoutes
+  ]
+})
+export class HomeModule { }
