726382c65013cc3898461eee12a17628cb0e5477
akshayn10 <thiruakshayan98@gmail.com>
2022-02-15 12:49:46 +0530

    signup 2 pages

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutes } from './auth-routing.module';
-
-
+import { Register1Component } from './register1/register1.component';
+import { RouterModule } from '@angular/router';
+import { Register2Component } from './register2/register2.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    Register1Component,
+    Register2Component
+
   ],
   imports: [
     CommonModule,
-    AuthRoutes
+    AuthRoutes,
+    RouterModule
   ],
   exports: [
   ]
---
+++ src/app/modules/auth/register1/register1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register1',
+  templateUrl: './register1.component.html',
+  styleUrls: ['./register1.component.css']
+})
+export class Register1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
