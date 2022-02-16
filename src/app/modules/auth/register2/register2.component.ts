9b2fdc54326c6a6fdfcb87fceb6c81dd6f905732
akshayn10 <thiruakshayan98@gmail.com>
2022-02-16 09:38:19 +0530

    reactive form for register2

--- src/app/modules/auth/register2/register2.component.ts
+++ src/app/modules/auth/register2/register2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register2',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register2.component.css']
 })
 export class Register2Component implements OnInit {
+  registerForm2: FormGroup = new FormGroup({});
+  submitted=false;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm2 = this.formBuilder.group({
+      description: ['',[ Validators.required]],
+      address1: ['', [Validators.required]],
+      address2: ['',[ Validators.required]],
+      city:['',[Validators.required]],
+      state:['',[Validators.required]],
+      country:['',[Validators.required]],
+      plan:['',[Validators.required]],
+      planCycle:['',[Validators.required]],
+    })
   }
 
+  get f() { return this.registerForm2.controls; }
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm2.invalid) {
+        return;
+    }
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm2.value))
+}
 }
