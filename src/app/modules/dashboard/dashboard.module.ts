4bfa741d0a68f3bb21b93775f914c243e8a7837f
akshayn10 <thiruakshayan98@gmail.com>
2022-02-20 20:48:12 +0530

    folder error fixed

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,7 +4,7 @@ import { DashboardTitleComponent } from './dashboard-title/dashboard-title.compo
 import { SharedModule } from '../../shared/shared.module';
 import { DashboardComponent } from './dashboard-main/dashboard.component';
 import { DashboardRoutes } from './dashboard-routing.module';
-import { DashboardTableComponent } from './dashboard-table/dashboard-table/dashboard-table.component';
+import { DashboardTableComponent } from './dashboard-table/dashboard-table.component';
 import { GraphComponent } from './graph/graph.component';
 import { ChartsModule } from 'ng2-charts';
 import { DashPieComponent } from './dash-pie/dash-pie.component';
