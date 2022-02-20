6ff155b969a0d7dc3ca54013425a59862f4c1b0f
akshayn10 <thiruakshayan98@gmail.com>
2022-02-20 15:05:35 +0530

    Subs page designed

---
+++ src/app/modules/subscription/sub-grid/sub-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-grid',
+  templateUrl: './sub-grid.component.html',
+  styleUrls: ['./sub-grid.component.css']
+})
+export class SubGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/subscription/sub-table/sub-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubTableComponent } from './sub-table.component';
+
+describe('SubTableComponent', () => {
+  let component: SubTableComponent;
+  let fixture: ComponentFixture<SubTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SubTableComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
